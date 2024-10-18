7021ad40d64a5d8e2909e02d495f1e0aa2e06398
yongjian520yehui <1447184083@qq.com>
2024-10-18 16:56:35 +0800

    1

--- ReincarnationJudge/judge_sence/book_ghost.gd
+++ ReincarnationJudge/judge_sence/book_ghost.gd
@@ -2,9 +2,16 @@ extends Control
 
 @onready var v_box_container = $VBoxContainer
 @onready var book_ghost = $"."
-@export var fade_in_duration := 0.3
-@export var fade_out_duration := 0.2
+@export var fade_in_duration := 1
+@export var fade_out_duration := 1
 @onready var ghost_name = %Name
+@onready var country: Label = %Country
+@onready var intro: Label = %Intro
+@onready var evaluation: Label = %Evaluation
+@onready var reason: Label = %Reason
+@onready var applay: Label = %Applay
+@onready var religion: Label = %Religion
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,12 +24,32 @@ func _process(_delta: float) -> void:
 
 
 func _on_close_pressed() -> void:
-	book_ghost.visible = false
+	book_ghost.close()
+
+func close() -> void:
+	var tween := create_tween()
+	get_tree().paused = false
+	tween.tween_property(
+		self,
+		^"modulate:a",
+		0.0,
+		fade_out_duration
+	).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
+	#tween.tween_property(
+		#book_ghost,
+		#^"anchor_bottom",
+		#0.5,
+		#fade_out_duration
+	#).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	tween.tween_callback(hide)
 
 func open() -> void:
-	var ghost_data = Utils.load_json_file("res://ghost_data/nomal_ghost.json")
-	##var ghost_name = book_ghost.get_node("%name")
-	ghost_name.text = ghost_data["day1"][0]["baseInfo"]["name"]
+	var ghost_json_data = Utils.ghost_factory()
+
+	ghost_name.text = ghost_json_data["day1"][0]["baseInfo"]["name"]
+	country.text = ghost_json_data["day1"][0]["baseInfo"]["country"]
+	religion.text = ghost_json_data["day1"][0]["baseInfo"]["religion"]
+	
 	show()
 	#resume_button.grab_focus()
 
--- ReincarnationJudge/judge_sence/judge_sence.gd
+++ ReincarnationJudge/judge_sence/judge_sence.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
-
-@onready var files: Button = $CanvasLayer/files
+@onready var files_button: Button = $CanvasLayer/FilesButton
+@onready var ghost_book_button: Button = $CanvasLayer/GhostBookButton
 @onready var pause_menu: PauseMenu = %PauseMenu
 @onready var book_ghost: Control = $CanvasLayer/book_ghost
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
-
 
 
 # Called when the node enters the scene tree for the first time.
@@ -18,8 +17,18 @@ func _process(_delta: float) -> void:
 	pass
 
 
-func _on_button_pressed() -> void:
-	book_ghost.visible=true
-	book_ghost.open()
-	animation_player.play("files_get")
+func _on_next_button_pressed() -> void:
+	if ghost_book_button.visible== false:
+		ghost_book_button.visible=true
+		animation_player.play("ghost_book_get")
 
+
+func _on_judge_button_pressed() -> void:
+	if book_ghost.visible== false:
+		ghost_book_button.visible= false
+
+
+func _on_ghost_book_button_pressed() -> void:
+	if book_ghost.visible== false:
+		book_ghost.visible=true
+		book_ghost.open()
