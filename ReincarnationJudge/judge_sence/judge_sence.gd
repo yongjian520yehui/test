d9fcc5ce9442f7f39845f162fb12a17c48fa8e83
yongjian520yehui <1447184083@qq.com>
2024-10-18 18:50:49 +0800

    a

--- ReincarnationJudge/judge_sence/judge_sence.gd
+++ ReincarnationJudge/judge_sence/judge_sence.gd
@@ -5,11 +5,12 @@ extends Node2D
 @onready var pause_menu: PauseMenu = %PauseMenu
 @onready var book_ghost: Control = $CanvasLayer/book_ghost
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
+@onready var death_god: Sprite2D = $DeathGod
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animation_player.play("death_god_idle")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
